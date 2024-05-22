10fd6cb76f784f878f306f2ead50cc10f08aa542
TheMegaForge <forgingsmeltery06@gmail.com>
2024-05-22 14:30:17 +0200

    modified for debug

--- src/sysInit.c
+++ src/sysInit.c
@@ -8,11 +8,12 @@ SETUP void sysInit(){
     bar.progress = 0;
     vbeInitBlockyProgressBar(&bar,30,getVBEHeight()-50,getVBEWidth()-60,30);
     display(bar.advancementSize);
-    uint64_t* ptr = PAGE_0x66A000_STORE_PT; 
     user_ptr list[] = {0x975000/*pml4*/,0x976000/*pdpt*/,0x977000/*pdt*/};
-    for(int i = 0;i<787;i++){
-        VirtualPage4K(&list,0xFFFFFFFF00000000+(i*0x1000),0x66A000+(i*0x1000),PFLAG_PRESENT|PFLAG_WRITE);
-    }
+    VirtualPage4K(&list,0xFFFFFFFF00000000,0x66A000,PFLAG_PRESENT|PFLAG_WRITE);
+
+    //for(int i = 0;i<787;i++){
+    //    VirtualPage4K(&list,0xFFFFFFFF00000000+(i*0x1000),0x66A000+(i*0x1000),PFLAG_PRESENT|PFLAG_WRITE);
+    //}
     setVBEScalar(1);
     vbeBlockyBarProgressBarAdvance(RGB(0,255,0),&bar);//shows that the system started
 }
